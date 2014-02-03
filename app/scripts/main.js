920a9ff68ecf8ac89aebf95706271f20f65c55a5
petechappell <peter@experiencebureau.com>
2014-02-03 22:34:21 +1000

    Pulling table from Wikipedia

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+  var $content = $('#tally_content'),
+      url = 'http://en.wikipedia.org/w/api.php?action=parse&page=2010_Winter_Olympics_medal_table&prop=text&section=1&format=json&callback=?',
+      $contentArray,
+      $medalTable;
+
+  $.getJSON(url, function(data) {
+    $contentArray = $(data.parse.text['*']);
+    console.log('content array length', $contentArray.length);
+    $contentArray.each(function(index) {
+      var $this = $(this);
+      console.log(index + ': ' + $this);
+      if ($this.is('table.wikitable')) {
+        $medalTable = $this;
+        return false;
+      }
+    });
+
+    $content.html($medalTable);
+  });
+
+});
